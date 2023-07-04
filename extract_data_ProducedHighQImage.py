c500dc3765d002e37020c77150e588e2c43a769d
Muhammad Sujak MK <anack.magja@gmail.com>
2023-07-04 18:43:11 +0900

    Added some feature

--- extract_data_ProducedHighQImage.py
+++ extract_data_ProducedHighQImage.py
@@ -1,22 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate
-from PIL import Image
+from PIL import ImageFont, ImageDraw, Image
+import cv2
 import matplotlib as mpl
-import time
+import os
 
 def path():
     return r"C:\RESEARCH\RESEARCH MOL\Surface Lattice\2D lattice Strucuture\Lattice with Waveguide\\"
 
 def main(): 
-    idx = 2 
+    idx = 3 
     if idx==2:
         label = "Reflectance"
     elif idx==3:
         label = "Transmittance"
     else:
         label = "Absorbance"
-    Dn = "array_water_TM_smallAOI_0_10" 
+    Dn = "array_water_TE_smallAOI_0_10" 
     filename= path()+f"Au_{Dn}.txt"
     file_Out= path()+f"Au_{Dn}_{label}.txt"
     file_dispersion= path()+f"Au_{Dn}_{label}_dispersion.txt"
@@ -24,7 +25,7 @@ def main():
      
     writing_file_dispersion(filename,file_dispersion,idx)
     maxx, minn = find_max_min(file_dispersion)
-    plot2D(file_dispersion,label,round(maxx,1),round(minn,1))
+    plot2D(file_dispersion,maxx,minn,label)
     #Separate_file(filename,idx,label,Dn)
     #MergeImage(Dn)
     AOI=np.linspace(0,10,11)
@@ -80,7 +81,7 @@ def writing_file_dispersion(filename,file_Out,idx):
         with open(file_Out,"w") as file:
             for line in lines:
                 line=line.split()
-                data = "{} {} {}\n".format(2*np.pi/float(line[1])*np.sin(np.pi/180*float(line[0]))*1e3,1240/float(line[1]),float(line[idx]))
+                data = "{} {} {}\n".format(2*np.pi/float(line[1])*np.sin(np.pi/180*float(line[0])),1240/float(line[1]),float(line[idx]))
                 #data = "{} {} {}\n".format((line[0]),line[1],line[idx])
                 file.write(data)
     file.close()
@@ -99,13 +100,13 @@ def Rayleigh_Anomaly(n,m):
     res1 = p/m 
     res2 = (n+np.sin(AOI*np.pi/180))*res1
     res3 = (n-np.sin(AOI*np.pi/180))*res1
-    kx1 = 2*np.pi/res2*np.sin(np.pi/180*AOI)*1e3
-    kx2 = 2*np.pi/res3*np.sin(np.pi/180*AOI)*1e3
+    kx1 = 2*np.pi/res2*np.sin(np.pi/180*AOI)
+    kx2 = 2*np.pi/res3*np.sin(np.pi/180*AOI)
     Erg1 = 1240/res2 
     Erg2 = 1240/res3 
     return kx1,Erg1,kx2,Erg2
 
-def plot2D(filename,label,maxx,minn):
+def plot2D(filename,maxx,minn,label):
     x, y, z = np.genfromtxt(filename,unpack=True)
     xi = np.linspace(x.min(), x.max(), 1000)
     yi = np.linspace(y.min(), y.max(), 1000)
@@ -119,61 +120,61 @@ def plot2D(filename,label,maxx,minn):
     #kx1m2sub,Erg1m2sub,kx2m2sub,Erg2m2sub = Rayleigh_Anomaly(nRIsub,m2)  
     kx1sup,Erg1sup,kx2sup,Erg2sup = Rayleigh_Anomaly(nRIsup,m1)  
     #kx1m2sup,Erg1m2sup,kx2m2sup,Erg2m2sup = Rayleigh_Anomaly(nRIsup,m2)  
-    plt.figure(figsize=(8,6))
-    im = plt.contourf(xi,yi,zi,levels=np.linspace(minn,maxx,200),cmap="Greys",extend='neither')
+    plt.figure(figsize=(9,7))
+    plt.subplots_adjust(left=.1, right=1.05, top=.95, bottom=.11) 
+    im = plt.contourf(xi,yi,zi,levels=np.linspace(minn,maxx,200),cmap="binary",extend='neither')
     cbar = plt.colorbar()
-    cbar.set_ticks([minn,maxx, 1]) 
-    cbar.ax.tick_params(labelsize=15)
+    ticks = np.linspace(minn,maxx,num=2)
+    formatted_ticks = ["{:.1f}".format(tick) for tick in ticks]
+    cbar.set_ticks(ticks)
+    cbar.set_ticklabels(formatted_ticks)
+    cbar.ax.tick_params(labelsize=20)
     plt.gca().set_facecolor('black')
-    plt.title('{}'.format(label), fontsize=20)
-    plt.xlabel('k$_{//}$ ($\u03BCm^{-1}$)', fontsize=15)
-    plt.ylabel('Energy (eV)', fontsize=15)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.plot(kx1sub,Erg1sub,"--",label="$\u03bb_{rayleigh}^{substrate} (+1)$",linewidth=1,c="k") 
-    plt.plot(kx2sub,Erg2sub,"--",label="$\u03bb_{rayleigh}^{substrate} (-1)$",linewidth=1,c="w") 
+    #plt.title('{}'.format(label), fontsize=20)
+    #plt.xlabel('k$_{//}$ ($\u03BCm^{-1}$)', fontsize=15)
+    plt.xlabel('k$_{//}$ ($nm^{-1}$)', fontsize=20)
+    plt.ylabel('Photon Energy (eV)', fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    lw = 3
+    plt.plot(kx1sub,Erg1sub,"--",label="$\u03bb_{rayleigh}^{substrate} (+1)$",linewidth=lw,c="w") 
+    plt.plot(kx2sub,Erg2sub,"--",label="$\u03bb_{rayleigh}^{substrate} (-1)$",linewidth=lw,c="k") 
     #plt.plot(kx1m2sub,Erg1m2sub,"--",label="$\u03bb_{rayleigh}^{substrate} (+2)$",linewidth=3) 
     #plt.plot(kx2m2sub,Erg2m2sub,"--",label="$\u03bb_{rayleigh}^{substrate} (-2)$",linewidth=3) 
-    plt.plot(kx1sup,Erg1sup,label="$\u03bb_{rayleigh}^{superstrate} (+1)$",linewidth=1,c="k") 
-    plt.plot(kx2sup,Erg2sup,label="$\u03bb_{rayleigh}^{superstrate} (-1)$",linewidth=1,c="w") 
+    plt.plot(kx1sup,Erg1sup,label="$\u03bb_{rayleigh}^{superstrate} (+1)$",linewidth=lw,c="w") 
+    plt.plot(kx2sup,Erg2sup,label="$\u03bb_{rayleigh}^{superstrate} (-1)$",linewidth=lw,c="k") 
     #plt.plot(kx1m2sup,Erg1m2sup,label="$\u03bb_{rayleigh}^{superstrate} (+2)$",linewidth=3) 
     #plt.plot(kx2m2sup,Erg2m2sup,label="$\u03bb_{rayleigh}^{superstrate} (-2)$",linewidth=3) 
-    plt.legend(loc='lower right',fontsize=13) 
-    plt.xticks(np.arange(0,2.1,0.5)) 
+    plt.legend(loc='lower right',fontsize=17) 
+    #plt.xticks(np.arange(0,2.1,0.5)) 
+    plt.xticks(np.arange(0e-3,2.1e-3,0.5e-3)) 
     plt.xlim(x.min(),x.max()) 
     plt.ylim(y.min(),y.max()) 
     #plt.savefig("Px Pol-Wavelength= {}nm.jpg".format(wl))
     plt.show()
+    os.remove(filename)
 
 def interPol(x,y):
     return scipy.interpolate.interp1d(x,y,'cubic')
 
-def MergingImage(Dn):
-    img_01 = Image.open("{} P = 300.0nm.jpg".format(Dn))
-    img_02 = Image.open("{} P = 350.0nm.jpg".format(Dn))
-    img_03 = Image.open("{} P = 400.0nm.jpg".format(Dn))
-    img_04 = Image.open("{} P = 450.0nm.jpg".format(Dn))
-    img_05 = Image.open("{} P = 500.0nm.jpg".format(Dn))
-    img_06 = Image.open("{} P = 550.0nm.jpg".format(Dn))
-    img_07 = Image.open("{} P = 600.0nm.jpg".format(Dn))
+def MergingImage():
+    path=r"C:\RESEARCH\RESEARCH MOL\Surface Lattice\2D lattice Strucuture\Lattice with Waveguide\Images\FOR PAPER\\"
+    img_01 = Image.open(path+"Transmittance_Water_TE.PNG")
+    img_02 = Image.open(path+"Transmittance_Water_TM.PNG")
+    img_03 = Image.open(path+"Transmittance_Glycerol_TE.PNG")
+    img_04 = Image.open(path+"Transmittance_Glycerol_TM.PNG")
     
     img_01_size = img_01.size
     img_02_size = img_02.size
     img_03_size = img_03.size
     img_04_size = img_04.size
-    img_05_size = img_05.size
-    img_06_size = img_06.size
-    img_07_size = img_07.size
     
-    new_im = Image.new('RGB',(4*img_01_size[0],2*img_01_size[1]))
+    new_im = Image.new('RGBA',(2*img_01_size[0],2*img_01_size[1]),(250,250,250))
     new_im.paste(img_01,(0,0))
     new_im.paste(img_02,(img_01_size[0],0))
-    new_im.paste(img_03,(2*img_01_size[0],0))
-    new_im.paste(img_04,(3*img_01_size[0],0))
-    new_im.paste(img_05,(0,img_01_size[1]))
-    new_im.paste(img_06,(img_01_size[0],img_01_size[1]))
-    new_im.paste(img_07,(2*img_01_size[0],img_01_size[1]))
-    new_im.save("Merge_Image_{}.png".format(Dn),"PNG") 
+    new_im.paste(img_03,(0,img_01_size[1]))
+    new_im.paste(img_04,(img_01_size[0],img_01_size[1]))
+    new_im.save(path+"Merge_Image.png",quality=95) 
     new_im.show() 
 
 def wvl2eV(wvl):
@@ -202,7 +203,7 @@ def find_max_min(file):
     """
 
 
-#MergeImage()
+#MergingImage()
 main()
 #find_max_min()
 
